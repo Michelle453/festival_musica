bc333f0266d1eb617da25dc4c3f266577e25463d
Michelle <mberriosanchez@gmail.com>
2025-07-15 19:34:21 -0500

    Cambios en el header

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,23 @@
 document.addEventListener('DOMContentLoaded', function(){
+    navegacionFija()
     crearGaleria()
+    resaltarEnlace()
+    scrollNav()
 })
+
+function navegacionFija(){
+    const header = document.querySelector('.header')
+
+    const sobreFestival = document.querySelector('.sobre-festival')
+
+    window.addEventListener('scroll', function(){
+        if(sobreFestival.getBoundingClientRect().bottom < 1){
+           header.classList.add('fixed')
+        }else{
+             header.classList.remove('fixed')
+        }
+    })
+}
 
 function crearGaleria(){
     const galeria = document.querySelector('.galeria-imagenes')
@@ -60,4 +77,44 @@ function cerrarModal(){
     },500)
     
 
+}
+
+function resaltarEnlace(){
+    document.addEventListener('scroll', function(){
+        const section = document.querySelectorAll('section')
+        const navLinks = this.documentElement.querySelectorAll('.navegacion-principal a')
+
+        let actual = ''
+        section.forEach(section =>{
+            const sectionTop = section.offsetTop
+            const sectionHeight = section.clientHeight
+
+            if(window.scrollY >= (sectionTop - sectionHeight /3)){
+                actual = section.id
+            }
+        })
+
+        navLinks.forEach(link =>{
+            link.classList.remove('active')
+            if(link.getAttribute('href')== '#'+actual){
+                link.classList.add('active')
+            }
+        })
+    })
+}
+
+function scrollNav(){
+    const navLinks = document.querySelectorAll('.navegacion-principal a')
+
+    navLinks.forEach(link=>{
+        link.addEventListener('click',e =>{
+            e.preventDefault()
+
+            const sectionScroll = e.target.getAttribute('href')
+
+            const section = document.querySelector(sectionScroll)
+
+            section.scrollIntoView({behavior:'smooth'})
+        })
+    })
 }
